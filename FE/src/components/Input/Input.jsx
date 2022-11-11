2f51f357823a31f98cdd65b42d09542139942abe
lihongfu25 <phuhd2k1@gmail.com>
2022-11-11 11:06:13 +0700

    [Refactor]: Input

--- FE/src/components/Input/Input.jsx
+++ FE/src/components/Input/Input.jsx
@@ -3,23 +3,21 @@ import { Box, InputBase } from "@mui/material";
 
 const Input = ({ startIcon, sx, ...props }) => {
     const Icon = startIcon;
-    if (Icon) return <Box>asds</Box>;
-    else
-        return (
-            <InputBase
-                {...props}
-                sx={{
-                    border: "1px solid #ccc",
-                    borderRadius: 1,
-                    fontSize: "1.6rem",
-                    "& input": {
-                        px: "1.2rem",
-                        py: "0.8rem",
-                    },
-                    ...sx,
-                }}
-            />
-        );
+    return (
+        <InputBase
+            {...props}
+            sx={{
+                border: "1px solid #ccc",
+                borderRadius: 1,
+                fontSize: "1.6rem",
+                "& input": {
+                    px: "1.2rem",
+                    py: "0.8rem",
+                },
+                ...sx,
+            }}
+        />
+    );
 };
 
 export default React.memo(Input);
