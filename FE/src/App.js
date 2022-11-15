2131bbbc2589a9e585efd1b0bc246eccdc384051
lihongfu25 <phuhd2k1@gmail.com>
2022-11-15 20:21:18 +0700

    update doc

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { useRoutes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useRoutes, useLocation, useNavigate } from "react-router-dom";
 import { routes } from "./route";
 function App() {
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
     const appRoutes = useRoutes(routes);
+    const { pathname } = useLocation();
+    React.useEffect(() => {
+        window.scrollTo(0, 0);
+        // if (user.role === 0) navigate("/manager");
+    }, [pathname, user]);
+
     return <div>{appRoutes}</div>;
 }
 
