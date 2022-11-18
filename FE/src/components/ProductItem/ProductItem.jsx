1e3ec6363288dfac658b763f494d87d2fffe36ed
lihongfu25 <phuhd2k1@gmail.com>
2022-11-18 18:12:27 +0700

    [Refactor]: ProductItem

--- FE/src/components/ProductItem/ProductItem.jsx
+++ FE/src/components/ProductItem/ProductItem.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { FaCartPlus } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import { Box, Rating } from "@mui/material";
-import { Button } from "../Button";
 import { currencyFormat } from "../../styles/GlobalStyles";
 const ProductItem = ({ product }) => {
+    const navigate = useNavigate();
     React.useEffect(() => {
         const img = document.querySelector(".product-img");
         img.style.height = img.clientWidth + "px";
     });
+    const handleGetProductDetail = (e) => {
+        navigate(`/product/detail/${product.productId}`);
+    };
     return (
         <Box
+            onClick={handleGetProductDetail}
             sx={{
+                width: "100%",
                 cursor: "pointer",
                 overflow: "hidden",
                 backgroundColor: "#fff",
@@ -21,47 +26,16 @@ const ProductItem = ({ product }) => {
                     transition: "all 0.3s ease-in-out",
                 },
                 ":hover .product-img": {
-                    transform: "scale(01.1)",
-                },
-                ":hover .product-actions": {
-                    display: "flex",
+                    transform: "scale(1.1)",
                 },
             }}
         >
             <Box
                 sx={{
                     overflow: "hidden",
-                    position: "relative",
                 }}
             >
                 <img className='product-img' alt='' src={product.img} />
-                <Box
-                    className='product-actions'
-                    sx={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        bottom: 0,
-                        display: "none",
-                        alignItems: "center",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                    }}
-                >
-                    <Button
-                        sx={{
-                            p: "2rem",
-                            fontSize: "2.4rem",
-                            backgroundColor: "rgba(0, 0, 0, 0.4)",
-                            "& > svg": {
-                                color: "#fff",
-                            },
-                        }}
-                    >
-                        <FaCartPlus />
-                    </Button>
-                </Box>
             </Box>
             <Box
                 sx={{
@@ -100,7 +74,8 @@ const ProductItem = ({ product }) => {
                     <span className='new-price'>
                         {product.discount !== 0
                             ? currencyFormat(
-                                  (product.price * product.discount) / 100,
+                                  (product.price * (100 - product.discount)) /
+                                      100,
                               )
                             : currencyFormat(product.price)}
                     </span>
